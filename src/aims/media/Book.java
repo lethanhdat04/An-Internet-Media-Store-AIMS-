86a7df0adf353081dfd2fb20d5f16e512e4e02ea
LE THANH DAT <lethanhdathd2908@gmail.com>
2024-12-01 23:35:05 +0700

    “Add a Book class”

---
+++ src/aims/media/Book.java
@@ -0,0 +1,84 @@
+package aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<>();
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Book(String title, float cost) {
+        this(title);
+        this.cost = cost;
+    }
+
+    public Book(String title, String category, float cost) {
+        this(title, cost);
+        this.category = category;
+    }
+
+    public boolean addAuthor(String author) {
+        for (String s : authors) {
+            if (Objects.equals(s, author)) {
+                System.out.println("Those authors existed");
+                return false;
+            }
+        }
+        authors.add(author);
+        System.out.println("Add author has completed");
+        return true;
+    }
+
+    public boolean removeAuthor(String author) {
+        for (String s : authors) {
+            if (Objects.equals(s, author)) {
+                authors.remove(author);
+                System.out.println("Remove has completed");
+                return true;
+            }
+        }
+        System.out.println("This author is not in list");
+        return false;
+    }
+
+    //Getter and Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
